a59525a5b37785041f2b76dcd850da483c779187
num0005 <num0005@outlook.com>
2025-07-01 17:02:19 +0100

    Port KiUnlockDispatcherDatabase to c

--- nboxkrnl/ke/irql.cpp
+++ nboxkrnl/ke/irql.cpp
@@ -114,56 +114,74 @@ EXPORTNUM(161) VOID FASTCALL KfLowerIrql
 	__writeeflags(EFlags);
 }
 
-EXPORTNUM(163) __declspec(naked) VOID FASTCALL KiUnlockDispatcherDatabase
+EXPORTNUM(163) VOID FASTCALL KiUnlockDispatcherDatabase
 (
 	KIRQL OldIrql
 )
 {
-	ASM_BEGIN
-		ASM(mov eax, [KiPcr]KPCR.PrcbData.NextThread);
-		ASM(test eax, eax);
-		ASM(jz end_func); // check to see if a new thread was selected by the scheduler
-		ASM(cmp cl, DISPATCH_LEVEL);
-		ASM(jb thread_switch); // check to see if we can context switch to the new thread
-		ASM(mov eax, [KiPcr]KPCR.PrcbData.DpcRoutineActive);
-		ASM(jnz end_func); // we can't, so request a dispatch interrupt if we are not running a DPC already
-		ASM(push ecx);
-		ASM(mov cl, DISPATCH_LEVEL);
-		ASM(call HalRequestSoftwareInterrupt);
-		ASM(pop ecx);
-		ASM(jmp end_func);
-	thread_switch:
-		// Save non-volatile registers
-		ASM(push esi);
-		ASM(push edi);
-		ASM(push ebx);
-		ASM(push ebp);
-		ASM(mov edi, eax);
-		ASM(mov esi, [KiPcr]KPCR.PrcbData.CurrentThread);
-		ASM(mov byte ptr [esi]KTHREAD.WaitIrql, cl); // we still need to lower the IRQL when this thread is switched back, so save it for later use
-		ASM(mov ecx, esi);
-		ASM(call KeAddThreadToTailOfReadyList);
-		ASM(mov [KiPcr]KPCR.PrcbData.CurrentThread, edi);
-		ASM(mov dword ptr [KiPcr]KPCR.PrcbData.NextThread, 0); // dword ptr required or else MSVC will aceess NextThread as a byte
-		ASM(movzx ebx, byte ptr [esi]KTHREAD.WaitIrql);
-		ASM(call KiSwapThreadContext); // when this returns, it means this thread was switched back again
-		ASM(test eax, eax);
-		ASM(mov cl, byte ptr [edi]KTHREAD.WaitIrql);
-		ASM(jnz deliver_apc);
-	restore_regs:
-		// Restore non-volatile registers
-		ASM(pop ebp);
-		ASM(pop ebx);
-		ASM(pop edi);
-		ASM(pop esi);
-		ASM(jmp end_func);
-	deliver_apc:
-		ASM(mov cl, APC_LEVEL);
-		ASM(call KfLowerIrql);
-		ASM(call KiExecuteApcQueue);
-		ASM(xor ecx, ecx); // if KiSwapThreadContext signals an APC, then WaitIrql of the previous thread must have been zero
-		ASM(jmp restore_regs);
-	end_func:
-		ASM(jmp KfLowerIrql);
-	ASM_END
+	PKPRCB Prcb = KeGetCurrentPrcb();
+	PKTHREAD Thread, NextThread;
+	BOOLEAN PendingApc;
+
+	/* Make sure we're at synchronization level */
+	NT_ASSERT(KeGetCurrentIrql() == SYNCH_LEVEL);
+
+	/* Check if we have deferred threads */
+	KiCheckDeferredReadyList(Prcb);
+
+	/* Check if we were called at dispatcher level or higher */
+	if (OldIrql >= DISPATCH_LEVEL)
+	{
+		/* Check if we have a thread to schedule, and that no DPC is active */
+		if ((Prcb->NextThread) && !(Prcb->DpcRoutineActive))
+		{
+			/* Request DPC interrupt */
+			HalRequestSoftwareInterrupt(DISPATCH_LEVEL);
+		}
+
+		/* Lower IRQL and exit */
+		goto Quickie;
+	}
+
+	/* Make sure there's a new thread scheduled */
+	if (!Prcb->NextThread) goto Quickie;
+
+	/* Lock the PRCB */
+	KiAcquirePrcbLock(Prcb);
+
+	/* Get the next and current threads now */
+	NextThread = Prcb->NextThread;
+	Thread = Prcb->CurrentThread;
+
+	/* Set current thread's swap busy to true */
+	KiSetThreadSwapBusy(Thread);
+
+	/* Switch threads in PRCB */
+	Prcb->NextThread = NULL;
+	Prcb->CurrentThread = NextThread;
+
+	/* Set thread to running */
+	NextThread->State = Running;
+
+	/* Queue it on the ready lists */
+	KiQueueReadyThread(Thread, Prcb);
+
+	/* Set wait IRQL */
+	Thread->WaitIrql = OldIrql;
+
+	/* Swap threads and check if APCs were pending */
+	PendingApc = KiSwapContext(OldIrql, Thread);
+	if (PendingApc)
+	{
+		/* Lower only to APC */
+		KeLowerIrql(APC_LEVEL);
+
+		/* Deliver APCs */
+		KiExecuteApcQueue();
+		NT_ASSERT(OldIrql == PASSIVE_LEVEL);
+	}
+
+	/* Lower IRQl back */
+Quickie:
+	KeLowerIrql(OldIrql);
 }
